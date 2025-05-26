73967b2af85c00a2bec244f83394eafba4ca2486
ItMightBeKaraoke <code@itmightbekaraoke.com>
2025-05-26 17:11:14 -0500

    Bump version

--- kbp2video/__init__.py
+++ kbp2video/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__version__ = '0.1.15'
+__version__ = '0.1.16'
 
 # run should be available under kbp2video.run() but definitely should not 
 # clog up the namespace if someone does an import *
